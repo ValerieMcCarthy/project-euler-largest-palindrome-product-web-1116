298ff64d1fabe7f373a6a0e2b91e6d76c6295b0b
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-01-22 17:28:57 -0500

    Palindrome found.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'pry'
+gem 'learn-co', '~> 2.3', '>= 2.3.1'
--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+
+ def largest_palindrome_product
+    a = 999
+    b = 999
+    max_palindrome = 1
+    while a > 0
+      b = 999
+      while b > 0
+        product = a * b
+          if product.to_s == product.to_s.reverse
+            max_palindrome = product if product > max_palindrome
+          end
+        b -= 1
+      end
+      a -= 1
+    end
+    max_palindrome
+  end
